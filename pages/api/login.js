d9e3b494c726825118d618003ea3a6582d41785e
Sam.Donaghy-Bell.76008920 <Sam.Donaghy-Bell@microfocus.com>
2022-05-26 15:54:46 +0100

    continuing to build login api

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,4 +1,5 @@
-import { magicAdmin } from '../../lib/magic';
+import { magicAdmin } from "../../lib/magic";
+import jwt from "jsonwebtoken";
 
 export default async function login(req, res) {
   if (req.method === "POST") {
@@ -10,6 +11,18 @@ export default async function login(req, res) {
       const metadata = await magicAdmin.users.getMetadataByToken(didToken);
       console.log({ metadata });
 
+      //create JWT token
+      const token = jwt.sign({
+        ...metadata,
+          iat: Math.floor(Date.now() / 1000),
+          exp: Math.floor(Date.now() / 1000 + 7 * 24 * 60 * 60),
+          "https://hasura.io/jwt/claims": {
+            "x-hasura-allowed-roles": ["user", "admin"],
+            "x-hasura-default-role": "user",
+            "x-hasura-user-id": `${metadata.issuer}`,
+        },
+      }, 'thisisasecretthisisasecret452343212342');
+      console.log({token});
 
       res.send({ done: true });
     } catch (error) {
@@ -19,6 +32,5 @@ export default async function login(req, res) {
     }
   } else {
     res.send({ done: false });
-
   }
 }
