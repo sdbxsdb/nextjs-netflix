41cb27a3f58b136c2616016f0dc6a83cadd6f550
Sam.Donaghy-Bell.76008920 <Sam.Donaghy-Bell@microfocus.com>
2022-05-27 14:23:27 +0100

    login api refactored

--- pages/api/login.js
+++ pages/api/login.js
@@ -7,39 +7,32 @@ export default async function login(req, res) {
   if (req.method === "POST") {
     try {
       const auth = req.headers.authorization;
-      const didToken = auth ? auth.substr(7) : '';
+      const didToken = auth ? auth.substr(7) : "";
 
       //invoke magic to get did token
       const metadata = await magicAdmin.users.getMetadataByToken(didToken);
 
       //create JWT token
-      const token = jwt.sign({
-        ...metadata,
+      const token = jwt.sign(
+        {
+          ...metadata,
           iat: Math.floor(Date.now() / 1000),
           exp: Math.floor(Date.now() / 1000 + 7 * 24 * 60 * 60),
           "https://hasura.io/jwt/claims": {
             "x-hasura-allowed-roles": ["user", "admin"],
-            "x-hasura-default-role": "user", 
+            "x-hasura-default-role": "user",
             "x-hasura-user-id": `${metadata.issuer}`,
+          },
         },
-      }, process.env.JWT_SECRET);
+        process.env.JWT_SECRET
+      );
 
       //CHECK IF USER EXISITS
       const isNewUserQuery = await isNewUser(token, metadata.issuer);
-      if (isNewUserQuery) {
-        //create a new user
-        const createNewUserMutation = await createNewUser(token, metadata);
-        console.log({ createNewUserMutation });
-        // set the cookie
-        const cookie = setTokenCookie(token, ses);
-        console.log({ cookie });
-        res.send({done: true, msg: 'is a new user'});
-      } else {
-        //set the cookie
-        const cookie = setTokenCookie(token, res);
-        console.log({ cookie });
-        res.send({done: true, msg: 'not a new user'});
-      }
+      isNewUserQuery && (await createNewUser(token, metadata));
+      // set the cookie
+      setTokenCookie(token, ses);
+      res.send({ done: true });
     } catch (error) {
       res.status(500);
       console.log("Something went wrong login in", error);
