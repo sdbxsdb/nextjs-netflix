971ce0420eb40749516cc160647eb5760410586c
Sam.Donaghy-Bell.76008920 <Sam.Donaghy-Bell@microfocus.com>
2022-05-30 10:44:23 +0100

    clean up of stats and hasura files

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -4,21 +4,18 @@ import {findVideoIdByUser} from '../../lib/db/hasura';
 export default async function stats(req, res) {
  
     if (req.method === "POST") {
-      console.log({ cookies: req.cookies });
 
       try {
       const token = req.cookies.Token;
       if (!token) {
         res.status(403).send({});
       } else {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        console.log({ decoded })
+        const videoId = req.query.videoId;
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
 
-        const userId = 'did:ethr:0x087E2672CC179dC427E2342EA50fbaB2653C19b4';
-        const videoId = 'TcMBFSGVi1c';
+        const userId = decodedToken.issuer;
         const findVideoId = await findVideoIdByUser(token, userId, videoId);
-        console.log({findVideoId});
-        res.send({ msg: "it works", decoded, findVideoId });
+        res.send({ msg: "it works", decodedToken, findVideoId });
       }
     }
     catch (error) {
