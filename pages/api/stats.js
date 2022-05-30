d50e3fedab706881198b470f0ddc0b2744ea2b6b
Sam.Donaghy-Bell.76008920 <Sam.Donaghy-Bell@microfocus.com>
2022-05-30 14:59:08 +0100

    stats API built and working

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,46 +1,53 @@
-import jwt from 'jsonwebtoken';
-import {findVideoIdByUser, insertStats, updateStats} from '../../lib/db/hasura';
+import jwt from "jsonwebtoken";
+import {
+  findVideoIdByUser,
+  insertStats,
+  updateStats,
+} from "../../lib/db/hasura";
 
 export default async function stats(req, res) {
- 
-    if (req.method === "POST") {
-
-      try {
+  if (req.method === "POST") {
+    try {
       const token = req.cookies.Token;
+      
       if (!token) {
         res.status(403).send({});
       } else {
-        const videoId = req.query.videoId;
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-
-        const userId = decodedToken.issuer;
-        const doesStatsExist = await findVideoIdByUser(token, userId, videoId);
-        if (doesStatsExist) {
-          //update it 
-          const response = await updateStats(token, {
-            favourited: 0, 
+        const { videoId, favourited, watched = true } = req.body;
+        
+        if (videoId) {
+          const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+          const userId = decodedToken.issuer;
+          const doesStatsExist = await findVideoIdByUser(
+            token,
             userId,
-            watched: true,
             videoId
-          });
-          res.send({ msg: "it works", response });
-        } 
-        else {
-          // add it 
-          const response = await insertStats(token, {
-            favourited: 0, 
-            userId,
-            watched: false,
-            videoId: '_rV1rbRxdrg'
-          });
-          res.send({ msg: "it works", response });
+          );
 
-        } 
+          if (doesStatsExist) {
+            //update it
+            const response = await updateStats(token, {
+              favourited,
+              userId,
+              watched,
+              videoId,
+            });
+            res.send({ data: response });
+          } else {
+            // add it
+            const response = await insertStats(token, {
+              favourited,
+              userId,
+              watched,
+              videoId,
+            });
+            res.send({ data: response });
+          }
+        }
       }
-    }
-    catch (error) {
+    } catch (error) {
       console.error("Error occured / stats", error);
       res.status(500).send({ done: false, error: error?.message });
     }
-  } 
+  }
 }
